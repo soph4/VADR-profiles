bca5a2a4817be79106372a2352a0eff0f77e280a
soph4 <soph4@umbc.edu>
2019-07-26 20:00:10 -0400

    Make more general

--- filter.sh
+++ filter.sh
@@ -1,39 +1,29 @@
-
-#Command: sh 1-filter.sh <sqa file> <fasta file>
+#Command: sh 1-filter.sh <sqa file> <refseq_summary.txt file><fasta file>
+#refseq_summary file contains 2 columns (1st: refseq accession numner, 2nd: length of refseq)
 
 sqa=$1
+refseq_summary=$2
 #Step 1
 #Input: requires the .sqa.tbl file
 #Output: .txt files with all the sequences that passed in their individual refseq groups
-
-python3 ~husg/HCV-v2/scripts/seperate.py $1 
+python3 /panfs/pan1/infernal/sophia/HCV-v2/scripts/seperate.py $1 $2
 
 #Step 2
 #Input: .txt file
 #Output: reverse.txt
 for file in NC_*;
 do
-
 	sort -k2,2 -r -n ${file:0:9}.txt > ${file:0:9}.reverse.txt	
-
-done
-
-#Step 3
-#Input: .reverse.txt files
-#Output: .reverse.list.txt
-
-for file in *reverse*;
-do
 	echo ${file:0:9}.1 > ${file:0:9}.reverse.list.txt
 	awk '{print $1}' ${file:0:9}.reverse.txt >> ${file:0:9}.reverse.list.txt		
 	
 done
 
-#Step 4
+#Step 3
 #Input: .reverse.list.txt files
 #Output: .fa files
 
-fa=$2
+fa=$3
 for file in *reverse.list.txt;
 do
 
@@ -45,7 +35,6 @@ do
 		esl-sfetch -f $fa ${file:0:9}.reverse.list.txt > ${file:0:9}.fa 
 	fi
 done
-
 
 rm *reverse.txt
 rm *reverse.list.txt
